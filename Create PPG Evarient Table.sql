b97308e96566d7b83c715cf3eff6b39a20c2460c
Myles Lefkovitz <170239@piedmonthospital.org>
2017-01-05 11:49:38 -0500

    Change PPG Evariant Script to use new Provider Master table

--- Create PPG Evarient Table.sql
+++ Create PPG Evarient Table.sql
@@ -4,7 +4,7 @@ select [PhysicianEnterprise].[dbo].[EvarientData].* INTO [PhysicianEnterprise].[
 
 INSERT INTO [PhysicianEnterprise].[dbo].[Evarient_PPG Scorecard Data]
 Select [PhysicianEnterprise].[dbo].[EvarientData].* 
-from [PhysicianEnterprise].[dbo].[EvarientData] JOIN [PhysicianEnterprise].[dbo].[physicianMaster_Provider] ON [PhysicianEnterprise].[dbo].[EvarientData].Provider1_NPI = [PhysicianEnterprise].[dbo].[physicianMaster_Provider].[NPI]
+from [PhysicianEnterprise].[dbo].[EvarientData] JOIN [PhysicianEnterprise].[ProviderMaster].[Provider] ON [PhysicianEnterprise].[dbo].[EvarientData].Provider1_NPI = [PhysicianEnterprise].[ProviderMaster].[Provider].[ProviderNPIID]
 UNION
 select [PhysicianEnterprise].[dbo].[EvarientData].* 
-from [PhysicianEnterprise].[dbo].[EvarientData] JOIN [PhysicianEnterprise].[dbo].[physicianMaster_Provider] ON [PhysicianEnterprise].[dbo].[EvarientData].Provider2_NPI = [PhysicianEnterprise].[dbo].[physicianMaster_Provider].[NPI];
+from [PhysicianEnterprise].[dbo].[EvarientData] JOIN [PhysicianEnterprise].[ProviderMaster].[Provider] ON [PhysicianEnterprise].[dbo].[EvarientData].Provider2_NPI = [PhysicianEnterprise].[ProviderMaster].[Provider].[ProviderNPIID];
